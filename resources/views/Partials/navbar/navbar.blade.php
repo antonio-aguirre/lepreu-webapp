543869c4795c925c5ea863e5a9771d3d98072871
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-02 22:34:06 -0600

    Proyecto base

--- resources/views/Partials/navbar/navbar.blade.php
+++ resources/views/Partials/navbar/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top"
-          >Start Bootstrap</a
+          >Ayuda para zoom</a
         >
         <button
           class="navbar-toggler navbar-toggler-right"
