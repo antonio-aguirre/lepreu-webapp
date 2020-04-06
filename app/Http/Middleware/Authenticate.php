753d6551a803b0af42586f3ef0a52e20366f2248
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-06 04:00:28 -0500

    Edited navbar

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('start');
+            return route('login');
         }
     }
 }
