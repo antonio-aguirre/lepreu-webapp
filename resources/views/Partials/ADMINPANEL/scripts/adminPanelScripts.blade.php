27e973f51af4154d77cd74e29033c27ec3100af5
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-06 02:54:37 -0500

    Some aditions in admin panel

--- resources/views/Partials/ADMINPANEL/scripts/adminPanelScripts.blade.php
+++ resources/views/Partials/ADMINPANEL/scripts/adminPanelScripts.blade.php
@@ -7,3 +7,21 @@
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 <script src="{{global_asset('assets/demo/datatables-demo.js')}}"></script>
+<!-- Getbutton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+527352769817", // WhatsApp number
+            sms: "+527352769817", // Sms phone number
+            call_to_action: "¿Ayuda en el panel de admin?", // Call to action
+            button_color: "#A8CE50", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "sms,whatsapp", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /Getbutton.io widget -->
