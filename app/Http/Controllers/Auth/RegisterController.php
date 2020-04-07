c97c9347d86c528ccdd51812c23dd8b725f4467c
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-07 00:08:24 -0500

    Some changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
             'second_last_name' => ['required', 'string', 'max:100'],
             'username' => ['required', 'string', 'max:15'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'phone_number' => ['required','numeric','min:10'],
+            'phone_number' => ['required','numeric','size:11'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
