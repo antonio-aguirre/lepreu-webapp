ccf1c0da3a64ebfce080b460eab4fb2177115beb
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-06 21:19:40 -0500

    Added some migrations

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('last_name');
+            $table->string('second_last_name');
             $table->string('username');
             $table->string('email')->unique();
+            $table->string('phone_numbre')->nullable();
+            $table->string('typeUser');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/migrations/tenant/2020_04_07_013557_create_notices_table.php
+++ database/migrations/tenant/2020_04_07_013557_create_notices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInfoDataTable extends Migration
+class CreateNoticesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateInfoDataTable extends Migration
      */
     public function up()
     {
-        Schema::create('info_data', function (Blueprint $table) {
+        Schema::create('notices', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('value'); //identificador de lo que se guardará
-            $table->text('data'); // información que se guardará
-            $table->text('description'); //descripción de lo que se guardará
+            $table->string('name');
+            $table->text('description');
+            $table->text('status');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateInfoDataTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('info_data');
+        Schema::dropIfExists('notices');
     }
 }
