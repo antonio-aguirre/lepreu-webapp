8502a7a5680c30f6d5ca55c51853c2525a35abd4
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-07 00:21:02 -0500

    Some changes

--- resources/views/Partials/navbar/navbar.blade.php
+++ resources/views/Partials/navbar/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
   <div class="container">
     <a class="navbar-brand js-scroll-trigger" href="/"
-      >Apoyo para videoconferencias zoom</a
+      >Ayuda zoom</a
     >
     <button
       class="navbar-toggler navbar-toggler-right"
