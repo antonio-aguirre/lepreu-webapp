14e9177c34176387b56edcbe2c27e0ec138bac86
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-08 22:06:26 -0500

    Some changes

--- resources/views/Partials/modal_questions/modal_questions.blade.php
+++ resources/views/Partials/modal_questions/modal_questions.blade.php
@@ -11,7 +11,6 @@
 
             <form action="{{ url('/dudas-zoom') }}" method="post">
                 <div class="modal-body">
-
                     @csrf
                     <div class="col-lg-12 text-center" style="color:black;">
                         <label> ESCRIBA SU EDAD </label>
@@ -20,8 +19,50 @@
 
                     <div class="col-lg-12 text-center" style="color:black;">
                         <label> SELECCIONE SU TIPO DE DISPOSITVO CON EL QUE SE CONECTA A ZOOM</label>
-                        <input type="number" class="form-control" name="age" value="{{ old('age') }}" min="0" required>
-                    </div>
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck1" name="device1">
+                                    <label class="custom-control-label" for="customCheck1"><i class="fas fa-mobile"></i>Celular</label>
+                                </div>
+                            </div>
+
+                            <div class="row col-lg-4">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck2" name="device2">
+                                    <label class="custom-control-label" for="customCheck2"><i class="fas fa-tablet-alt"></i>Tablet</label>
+                                </div>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck3" name="device3">
+                                <label class="custom-control-label" for="customCheck3"><i class="fas fa-laptop"></i>Computadora</label>
+                            </div>
+                        </div>
+                    </div><hr><br>
+                    
+                    <div class="col-lg-12 text-center" style="color:black;">
+                        <label> SELECCIONE SU SISTEMA </label>
+
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck4" name="operative_system1">
+                                    <label class="custom-control-label" for="customCheck4"><i class="fab fa-android"></i>Android</label>
+                                </div>
+                            </div>
+
+                            <div class="row col-lg-4">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck5" name="operative_system2">
+                                    <label class="custom-control-label" for="customCheck5"><i class="fab fa-apple"></i>iOS</label>
+                                </div>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck6" name="operative_system3">
+                                <label class="custom-control-label" for="customCheck6"><i class="fab fa-windows"></i>Windows</label>
+                            </div>
+                        </div>
+                    </div><hr>
 
                     <div class="col-lg-12 text-center" style="color:black;">
                         
--- resources/views/infoPage.blade.php
+++ resources/views/infoPage.blade.php
@@ -3,11 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{global_asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{global_asset('css/main-page.css')}}" rel="stylesheet" />
+    <link href="{{global_asset('css/animate.css')}}" rel="stylesheet" />
+    <!-- Custom fonts for this template -->
+    <link href="{{global_asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{global_asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" />
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" />
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="{{global_asset('device-mockups/device-mockups.min.css')}}" />
+    <!-- Custom styles for this template -->
+    <link href="{{global_asset('css/new-age.min.css')}}" rel="stylesheet" />
     <title>Lepreu</title>
 </head>
-<body>
-
-    <h1> SOME GREAT DOING HERE! </h1>
-    
-</body>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container" style="padding-top: 10em;">
+                <h1 class="display-4 text-center">Lepreu</h1>
+                <p class="text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+    </body>
 </html>
