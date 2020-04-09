d49999e626e859f66b9e188d38dd5444972ebc31
ken <harygato3@gmail.com>
2020-04-08 20:54:36 -0500

    added meta tags for infoPage.blade php

--- resources/views/infoPage.blade.php
+++ resources/views/infoPage.blade.php
@@ -3,6 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{global_asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{global_asset('css/main-page.css')}}" rel="stylesheet" />
+    <!-- Custom fonts for this template -->
+    <link href="{{global_asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{global_asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" />
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" />
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="{{global_asset('device-mockups/device-mockups.min.css')}}" />
+    <!-- Custom styles for this template -->
+    <link href="{{global_asset('css/new-age.min.css')}}" rel="stylesheet" />
     <title>Lepreu</title>
 </head>
     <body>
