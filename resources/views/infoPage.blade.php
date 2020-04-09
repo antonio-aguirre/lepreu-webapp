fce7e3a9e6c1e780fc858feb5db59a77c4cfc2b2
ken <harygato3@gmail.com>
2020-04-08 20:18:13 -0500

    starting infopage design

--- resources/views/infoPage.blade.php
+++ resources/views/infoPage.blade.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lepreu</title>
 </head>
-<body>
-
-    <h1> SOME GREAT DOING HERE! </h1>
-    
-</body>
+    <body>
+        <img src="https://res.cloudinary.com/craftwebs/image/upload/v1586393392/WhatsApp_Image_2020-04-08_at_12.03.37_PM_psf7og.jpg" alt="" style="vertical-align: middle;border-style: none;width: 100%;height: 44em;object-fit: cover;">
+    </body>
 </html>
