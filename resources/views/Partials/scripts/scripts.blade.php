0bb54971adf3e01ec347a752a30dcd1acd81a3e3
ken <harygato3@gmail.com>
2020-04-08 22:03:50 -0500

    added init for wow.js

--- resources/views/Partials/scripts/scripts.blade.php
+++ resources/views/Partials/scripts/scripts.blade.php
@@ -1,10 +1,13 @@
 <!-- Bootstrap core JavaScript -->
 <script src="{{global_asset('vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{global_asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
 <!-- Plugin JavaScript -->
 <script src="{{global_asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
 <!-- Custom scripts for this template -->
 <script src="{{global_asset('js/new-age.min.js')}}"></script>
+<!-- wow.js animation library on scroll -->
+<script src="{{global_asset('js/wow.js')}}"></script>
+<script>
+    new WOW().init();
+</script>
 
