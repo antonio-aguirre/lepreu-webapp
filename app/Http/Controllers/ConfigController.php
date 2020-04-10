75f8c42eb12876b43437e0520d1bf49af9fcd944
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-10 01:54:48 -0500

    Some modifications in button to access to zoom

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\InfoData;
 use Session;
 use Stancl\Tenancy\Tenant;
-use DB;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,13 +14,12 @@ class ConfigController extends Controller
     public function configurations()
     {
         // recuperando el link de zoom para cargarlo en el boton
-        $link_zoom = DB::table('info_data')
-                                ->select('data')
+        $links = InfoData::select('data')
                                 ->where('value','LINK-ZOOM')
                                 ->where('status','TO USE')    
                                 ->get();                      
 
-        $dd = $link_zoom[0]->data;
-        return view('welcome',compact('dd'));
+        
+        return view('welcome',compact('links'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,12 @@
                             UNASE A LA REUNIÓN OPRIMIENDO EL BOTÓN DE ABAJO
                     </h1>                
                     
-                    <!-- 9513005367 -->
+                    @foreach($links as $link)
                     <a class="btn btn-primary btn-xl js-scroll-trigger btn-block" style=" text-align:center; display:block;"
-                                href="{{ url('https://us04web.zoom.us/j/'.$dd.'')}}">
+                                href="{{ url(''.$link->data.'')}}">
                         OPRIMA AQUÍ PARA UNIRSE A LA REUNIÓN
                     </a>
+                    @endforeach
 
                     <!------------------->
                     <br><br><br></br>
