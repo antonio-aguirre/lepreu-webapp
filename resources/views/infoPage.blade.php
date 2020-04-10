a021560ddd3bde81c32e62594998a494b46a5b25
ken <harygato3@gmail.com>
2020-04-09 20:40:01 -0500

    added js scripts for wow animation and css animation. Added blocks with images

--- resources/views/infoPage.blade.php
+++ resources/views/infoPage.blade.php
@@ -20,10 +20,40 @@
 </head>
     <body>
         <div class="jumbotron jumbotron-fluid">
-            <div class="container" style="padding-top: 10em;">
+            <div class="container wow fadeInUp" style="padding-top: 10em;">
                 <h1 class="display-4 text-center">Lepreu</h1>
-                <p class="text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <p class="text-center">No te quedes lejos de mí, porque se acercan dificultades y no tengo a nadie más que me ayude.</p>
             </div>
         </div>
+
+        <div class="section container ">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12 wow fadeInLeft" style="padding-top: 10em;">
+                    <h1>Bienvenido!</h1>
+                    <p>
+                        Este sitio está orientado a ayuda y soporte de la aplicación de Zoom. Está especialmente dirigido a 
+                        nuestros hermanos Testigos de Jehová que necesiten ayuda a usar la aplicación de Zoom.
+                        Toda la ayuda brindada aquí es sin fines de lucro.
+                    </p>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-12 wow fadeInRight">
+                    <img src="https://res.cloudinary.com/craftwebs/image/upload/v1586480537/zoom-phone_zlskb4.png" alt="" style="position: absolute;display: block;z-index: 100;padding-top: 6em;padding-left: 5em;" class="img-fluid">
+                    <img src="https://res.cloudinary.com/craftwebs/image/upload/v1586479539/blob-shape-wine_chegj1.svg" alt="" style="display: inline;position: absolute;z-index: -35;" class="img-fluid">
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <script src="{{global_asset('vendor/jquery/jquery.min.js')}}"></script>
+        <script src="{{global_asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{global_asset('js/wow.js')}}"></script>
+        <script>
+            new WOW().init();
+        </script>
+    
     </body>
 </html>
