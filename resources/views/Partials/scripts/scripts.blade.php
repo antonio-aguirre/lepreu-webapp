8a6480d0462a87b85a47d36b736e7d26015db51a
ken <harygato3@gmail.com>
2020-04-11 21:23:57 -0500

    added script js

--- resources/views/Partials/scripts/scripts.blade.php
+++ resources/views/Partials/scripts/scripts.blade.php
@@ -11,3 +11,15 @@
     new WOW().init();
 </script>
 
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js').then(function (registration) {
+                console.log('ServiceWorker registration :', registration.scope);
+            }).catch(function (error) {
+                console.log('ServiceWorker registration failed:', errror);
+            });
+        });
+    }
+</script>
+
