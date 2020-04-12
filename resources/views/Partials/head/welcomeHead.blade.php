8d1edecf530e133d2300e7ffa700bb593b42890f
ken <harygato3@gmail.com>
2020-04-11 21:24:12 -0500

    added manifst links and pwa meta tags

--- resources/views/Partials/head/welcomeHead.blade.php
+++ resources/views/Partials/head/welcomeHead.blade.php
@@ -1,7 +1,14 @@
  <head>
     <meta charset="utf-8" />
+    
+    <!--Meta tags for pwa -->
     <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="theme-color" content="#AE3361">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="msapplication-starturl" content="/">
+
     <title>@yield('page-title')</title>
 
     <!-- Bootstrap core CSS -->
