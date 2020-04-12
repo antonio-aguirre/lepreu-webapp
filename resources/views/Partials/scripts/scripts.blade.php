779614e61f72ffc4dba2075b39f5a0738a5f5482
ken <harygato3@gmail.com>
2020-04-12 17:38:17 -0500

    fixed the serrror on the scripts

--- resources/views/Partials/scripts/scripts.blade.php
+++ resources/views/Partials/scripts/scripts.blade.php
@@ -17,7 +17,7 @@
             navigator.serviceWorker.register('/sw.js').then(function (registration) {
                 console.log('ServiceWorker registration :', registration.scope);
             }).catch(function (error) {
-                console.log('ServiceWorker registration failed:', errror);
+                console.log('ServiceWorker registration failed:', error);
             });
         });
     }
