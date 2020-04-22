172bfed7468218bed2366a6541a5b1d6f77c2f88
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-22 01:35:33 -0500

    Changed color style in the leters

--- resources/views/Partials/sections/download.blade.php
+++ resources/views/Partials/sections/download.blade.php
@@ -1,4 +1,4 @@
-<section class="download bg-primary text-center" id="download">
+<section class="download  text-center section-downloads" id="download">
    
     <div class="container">
         <div style="background-color:red; border-radius:30px; box-shadow: 5px 10px 5px;">
@@ -12,7 +12,7 @@
         <div class="row badges">
 
             <div class="col-lg-4">
-                <h2 class="section-heading">Botón para descargar en Android</h2>
+                <h2 class="section-heading" style="color:#F7F7F7;">Botón para descargar en Android</h2>
                 
                 <a class="badge-link" href="https://play.google.com/store/apps/details?id=us.zoom.videomeetings">
                     <img src="{{ global_asset('img/google-play-badge.svg') }}" alt=""/>
@@ -21,7 +21,7 @@
             </div><br>
             
             <div class="col-lg-4">    
-                <h2 class="section-heading">Botón para descargar en iPhone, iPad</h2>
+                <h2 class="section-heading" style="color:#F7F7F7;">Botón para descargar en iPhone, iPad</h2>
 
                 <a class="badge-link" href="https://apps.apple.com/mx/app/zoom-cloud-meetings/id546505307">
                     <img src="{{ global_asset('img/app-store-badge.svg') }}" alt=""/>
@@ -30,7 +30,7 @@
             </div>
 
             <div class="col-lg-4">
-                <h2 class="section-heading">Botón para descargar en Kindle</h2>
+                <h2 class="section-heading" style="color:#F7F7F7;">Botón para descargar en Kindle</h2>
 
                 <a class="badge-link" href="https://www.amazon.com/-/es/dp/B00B5L5JRM/ref=sr_1_1?keywords=zoom&qid=1585813748&s=mobile-apps&sr=1-1">
                     <img src="{{ global_asset('img/amazon-badge.png') }}" alt=""/>
