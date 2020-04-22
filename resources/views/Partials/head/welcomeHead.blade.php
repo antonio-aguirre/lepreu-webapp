74a5db3d0bf3462d039578fc89332ef39b267fde
Antonio Aguirre <antonio_aar@hotmail.es>
2020-04-22 12:27:27 -0500

    change color theame

--- resources/views/Partials/head/welcomeHead.blade.php
+++ resources/views/Partials/head/welcomeHead.blade.php
@@ -3,7 +3,7 @@
     
     <!--Meta tags for pwa -->
     <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="theme-color" content="#AE3361">
+    <meta name="theme-color" content="#379271">
     <link rel="manifest" href="/manifest.json">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
