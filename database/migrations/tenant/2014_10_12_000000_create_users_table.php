c0cc501ab0ab21e06d68ffb270fd72184a983804
Antonio Aguirre <antonio_aar@hotmail.es>
2020-11-05 01:53:33 -0600

    Modified column email to nullable and renamed column lastName

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('last_name');
+            $table->string('lastName');
             $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->string('phone_number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
